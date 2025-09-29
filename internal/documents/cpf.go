984633abfaf9a69b8000d6b2754e6aa7793d87fb
George Rodrigues <georgerodrigues@live.com.au>
2025-09-28 22:53:03 -0300

    feat: implement cpf algorithm

--- internal/documents/cpf.go
+++ internal/documents/cpf.go
@@ -1,5 +1,74 @@
 package documents
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 func GenerateCpf() string {
-	return "22222222222"
+	var validCpf string
+	for {
+		generatedCpf := fmt.Sprintf("%011d", rand.Int63n(100000000000))
+
+		if isValidCpf(generatedCpf) {
+			validCpf = generatedCpf
+			break
+		}
+	}
+
+	return validCpf
+}
+
+func isValidCpf(document string) bool {
+	if len(document) != 11 {
+		return false
+	}
+	if allDigitsEqual(document) {
+		return false
+	}
+
+	var tempTotal, firstDigit, secondDigit int
+	var finalCpf string
+	for i := 0; i < 9; i++ {
+		tempTotal += int(document[i]-48) * (10 - i)
+	}
+
+	tempTotal = tempTotal % 11
+
+	if tempTotal < 2 {
+		firstDigit = 0
+	} else {
+		firstDigit = 11 - tempTotal
+	}
+
+	finalCpf = fmt.Sprintf("%s%d", document[:9], firstDigit)
+
+	tempTotal = 0
+	for i := 0; i < 10; i++ {
+		tempTotal += int(finalCpf[i]-48) * (11 - i)
+	}
+
+	tempTotal = tempTotal % 11
+
+	if tempTotal < 2 {
+		secondDigit = 0
+
+	} else {
+		secondDigit = 11 - tempTotal
+	}
+
+	finalCpf = fmt.Sprintf("%s%d", finalCpf, secondDigit)
+	if document == finalCpf {
+		return true
+	}
+	return false
+}
+
+func allDigitsEqual(s string) bool {
+	for i := 1; i < len(s); i++ {
+		if s[i] != s[0] {
+			return false
+		}
+	}
+	return true
 }
