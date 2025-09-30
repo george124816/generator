a5af2c14381e7e8a96e3f24fc046699355583baa
George Rodrigues <georgerodrigues@live.com.au>
2025-09-30 20:45:08 -0300

    fix: url on swagger

--- docs/docs.go
+++ docs/docs.go
@@ -35,7 +35,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:4000",
+	Host:             "generator.georgerodrigues.dev",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Generator API",
